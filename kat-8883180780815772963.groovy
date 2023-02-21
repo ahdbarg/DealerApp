9b0336d9785639dce458931cd8c9097712c75bd9
ahdbarg <125978004+ahdbarg@users.noreply.github.com>
2023-02-21 14:46:44 +0300

    Add files via upload

---
+++ kat-8883180780815772963.groovy
@@ -0,0 +1,15 @@
+import static org.assertj.core.api.Assertions.*
+
+import java.awt.print.PrinterGraphics
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
